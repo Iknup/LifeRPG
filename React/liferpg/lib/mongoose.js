04b4b04c0108e36a85bcd757dbbb909df5a8e529
Iknup <leesiyeol03@gmail.com>
2023-05-22 19:43:41 -0400

    model(not complete) utility mongodb update

--- React/liferpg/lib/mongoose.js
+++ React/liferpg/lib/mongoose.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 
+const uri = process.env.MONGODB_URI;
+
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(
-      'mongodb+srv://leesiyeol03:8dnViAfUBHnMdqfJ@cluster0.rcqcfe3.mongodb.net/?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const connection = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`Connected to MongoDB! ${connection.connection.host}`);
   } catch (e) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
